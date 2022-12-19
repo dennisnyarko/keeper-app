cccafb98e58eebbf6f85913fd57f157f9847c688
Dennis Nyarko <dennis_nyarko@outlook.com>
2022-12-19 17:53:23 +0000

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,12 +11,13 @@ function App() {
     return (
         <div>
             <Header />
-            {notes.map((noteItem) =>  <Note
+            {notes.map((noteItem) =>  (
+            <Note
             key={noteItem.key}
             title={noteItem.title}
             content={noteItem.content}
      />
-)}
+))}
             <Footer />
         </div>
         
